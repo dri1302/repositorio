9489de38413e4e2f06ce41cb45f8ef59dc3e00a6
CarlosCamuzzi <carloscamuzzi.hs@gmail.com>
2021-10-24 14:53:42 -0300

    Layout-Login
    Atualização de layout e tela login. Links Funcionais

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
-// FUNÇÕES PARA CONTATO E SOBRE:
-
-// Acessar menu contato
-function menuContato() {
-    const contato = document.querySelector('nav#contato');
-    alert('ENTROU MENU CONTATO EM menuContato()');
-}
-
-// Acessar menu sobre
-function menuSobre() {
-    const sobre = document.querySelector('nav#sobre');
-    alert('ENTROU MENU SOBRE EM MenuSobre()');
-}
-// FIM FUNÇÕES PARA CONTATO E SOBRE
-
-
-
-// FUNÇÕES PARA LOGIN E CADASTRO CLIENTE
-const log = document.querySelector('input#entrar')
-const cad = document.querySelector('input#cadastrar')
-
 // Entrar Login
 function entrarLogin() {
-    alert('ENTROU BOTÃO ENTRAR entrarLogin()');
+    const log = document.querySelector('input#bot-entrar')
+    alert('ENTROU FUNÇÃO ENTRAR: entrarLogin()');
 }
 
 // Cadastrar Cliente
 function cadastrarCliente() {
-    alert('ENTROU cadastrarCliente()');
+    const cad = document.querySelector('input#bot-cadastrar')
+    alert('ENTROU FUNÇÃO CADASTRO: cadastrarCliente()');
 }
 // FIM FUNÇÕES PARA LOGIN E CADASTRO CLIENTE
 
 
-/* rever
+
 // FUNÇÃO PARA ENVIO EMAIL PLANOS - BOTÃO INSCREVA-SE FOOTER
 function emailInscreva(){
-    const inscreva = document.querySelector('input#bot-inscreva');
-        alert('ENTROU ENVIO EMAIL INSCREVA-SE EM emailInscreva()')
+    const inscreva = document.querySelector('email-rodape#bot-inscreva'); // Class#ID
+        alert('ENTROU FUNÇÃO INSCREVA-SE: emailInscreva()')
 }
-// FIM FUNÇÃO PARA ENVIO EMAIL PLANOS - BOTÃO INSCREVA-SE FOOTER*/
+// FIM FUNÇÃO PARA ENVIO EMAIL PLANOS - BOTÃO INSCREVA-SE FOOTER
